cb6d5813ab07129202da70ab9196801d737bb36a
ehongzhen <ehongzhen@qq.com>
2016-01-06 17:32:40 +0800

    Initial Commit

---
+++ SimpleWebe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWebe
+//
+//  Created by 鄂鸿桢 on 16/1/6.
+//  Copyright © 2016年 ehongzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleWebe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleWebe
+//
+//  Created by 鄂鸿桢 on 16/1/6.
+//  Copyright © 2016年 ehongzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
